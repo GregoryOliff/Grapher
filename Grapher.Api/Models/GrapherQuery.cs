95887c6fc01216d9a64da332cfb3c01c76ccad61
Oliff <028989@aero.local>
2018-06-26 13:30:02 -0400

    Fixed GrapherQuery so queries for flights and aircraft are all in one place

--- Grapher.Api/Models/GrapherQuery.cs
+++ Grapher.Api/Models/GrapherQuery.cs
@@ -7,7 +7,7 @@ namespace Grapher.Api.Models
     {
         Field<AircraftType>(
             "aircraft",
-            arguments: new QueryArguments(new QueryArgument<IntGraphType> {Name = "id" }),
+            arguments: new QueryArguments(new QueryArgument<IntGraphType> {Name = "id"}),
             resolve: context => aircraftRepository.Get(context.GetArgument<int>("id")));
          
         Field<AircraftType>(
@@ -17,7 +17,20 @@ namespace Grapher.Api.Models
             
         Field<AircraftType>(
             "all",
-            resolve: context => playerRepository.All());    
+            resolve: context => playerRepository.All());
             
+        Field<FlightType>(
+            "flight",
+            arguments: new QueryArguments(new QueryArguments<IntGraphType> {Name = "id"}),
+            resolve: context => flightRepository.Get(context.GetArgument<int>("id")));
+         
+        Field<FlightType>(
+            "flightStartTime",
+            arguments: new QueryArguments(new QueryArgument<DateGraphType> {Name = "start"}),
+            resolve: context => flightRepository.Get(context.GetArgument<DateTime>("start")));
+            
+        Field<FlightType>(
+            "all",
+            resolve: context => flightRepository.All());
     }
 }
