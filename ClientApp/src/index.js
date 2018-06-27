463de43065ef73c21556d3fbff760b16ad984b5a
Oliff <028989@aero.local>
2018-06-27 15:35:21 -0400

    Added bootstrap, fixed some importing routes

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,9 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css'
+import '../../node-v8.11.3-win-x64/node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node-v8.11.3-win-x64/node_modules/bootstrap/dist/css/bootstrap-theme.css'
 import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from '../../node-v8.11.3-win-x64/node_modules/react';
+import ReactDOM from '../../node-v8.11.3-win-x64/node_modules/react-dom';
+import { BrowserRouter } from '../../node-v8.11.3-win-x64/node_modules/react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
