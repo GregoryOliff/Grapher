9e2a661a0fb30df258e8f42021e01cbe7be32d1d
Oliff <028989@aero.local>
2018-07-03 13:23:14 -0400

    Laid out framework for redux

---
+++ ClientApp/src/Redux/actions.js
@@ -0,0 +1,9 @@
+/*import React from '../../../Packages/node-v8.11.3-win-x64/node_modules/react';
+
+//actions types
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+//other constants
+export const VisibilityFilters = {
+    
+}
+//action creators*/
